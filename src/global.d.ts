7500aed7c1401e4d966fbae6dbe938284aebfc61
MagedElfar <maged.1992.me@gmail.com>
2026-03-10 22:41:29 +0200

    add category pages

--- src/global.d.ts
+++ src/global.d.ts
@@ -11,7 +11,8 @@ declare module 'next-intl' {
 }
 
 declare global {
-    type IntlMessages = Messages
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    interface IntlMessages extends Messages { }
 }
 
 export { };
